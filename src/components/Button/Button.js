dd9f15f38c4178060aef4271f7e0a3825f94f0e1
ttawan <ttawanc@gmail.com>
2018-04-23 13:38:16 +1000

    added style-system

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
+import { width, propTypes } from 'styled-system'
 
 const ButtonStyled = styled.div`
   border: 1px solid ${props => props.theme.button.borderColor};
+  ${width};
 `
 
 const Text = styled.label`
@@ -26,6 +28,7 @@ Button.propTypes = {
 
 Button.defaultProps = {
   children: 'Button Text',
+  ...propTypes.width,
 };
 
 export default Button;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 // components
 export { default as Button } from './components/Button'
 
+// Style component library
+import { default as styled } from 'styled-components'
+import { default as styledSystem } from 'styled-system'
+
 // Theme
 export { default as theme } from './theme'
 export { default as ThemeProvider } from './ThemeProvider'
